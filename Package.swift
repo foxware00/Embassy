56eb220598f32bf1cf7bff334215ee017fa43d0c
Oliver Fox <fox.oliverj@gmail.com>
2025-02-18 14:27:59 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,5 +4,5 @@ import PackageDescription
 let package = Package(
     name: "Embassy",
     products: [.library(name: "Embassy", targets: ["Embassy"])],
-    targets: [.target(name: "Embassy"), path: "./Sources"]
+    targets: [.target(name: "Embassy", path: "./Sources")]
 )
